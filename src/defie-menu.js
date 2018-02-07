70d32a791f5e99801eddb039b0b3b1cd11a7a14f
jason <zwang@fqsystems.com>
2018-02-07 10:07:33 -0800

    update

--- src/defie-menu.js
+++ src/defie-menu.js
@@ -908,7 +908,7 @@ export class DefieMenu extends PolymerElement {
         this.set('leftservices', e.detail.services)
         this.getSetting(sessionStorage.getItem("PR"))
 
-        this.$.serviceajax.generateRequest();
+        // this.$.serviceajax.generateRequest();
     }
 
 
