9eedd5387b6328d22cbc2de4ce9f38e72a1db510
Kyle <kmcgourty@defie.co>
2018-03-14 16:27:50 -0700

    shipping labels

--- src/defie-menu.js
+++ src/defie-menu.js
@@ -406,6 +406,9 @@ export class DefieMenu extends PolymerElement {
 
                 <cashreceipt-shell name="cashreceipt" setting="[[setting]]"></cashreceipt-shell>
 
+                <shippinglabels-shell name="shippinglabels" setting="[[setting]]"></shippinglabels-shell>
+
+
 
             </iron-pages>
             
@@ -583,6 +586,8 @@ export class DefieMenu extends PolymerElement {
                         "apcreditmemo": "apcreditmemos",
                         "barcodes-new": "barcodes",
                         "barcodes": "barcodes",
+                        "shippinglabels-new": "shippinglabels",
+                        "shippinglabels": "shippinglabels",
                     }
                 }
             },
@@ -647,9 +652,11 @@ export class DefieMenu extends PolymerElement {
                         "boms": "/boms/src/boms-shell.js",
                         "bom-new": "/boms/src/boms-shell.js",
                         "qachecklists": "/qachecklists/src/qachecklists-shell.js",
-                        "barcodes-new": "/barcodes/src/barcodes-shell.js",
-                        "barcodes": "/barcodes/src/barcodes-shell.js",
                         "qachecklist-new": "/qachecklists/src/qachecklists-shell.js",
+                           "barcodes-new": "/barcodes/src/barcodes-shell.js",
+                        "barcodes": "/barcodes/src/barcodes-shell.js",
+                        "shippinglabel-new": "/shippinglabels/src/shippinglabels-shell.js",
+                        "shippinglabels": "/shippinglabels/src/shippinglabels-shell.js",
                         "releasedboms": "/releasedboms/src/releasedboms-shell.js",
                         "releasebom-new": "/releasedboms/src/releasedboms-shell.js",
                         "buildorders": "/buildorders/src/buildorders-shell.js",
